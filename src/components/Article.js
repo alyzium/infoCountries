1a925c90beed7b202c067d19a8c9c0b58e0c2c1f
Idriss Semlali <isemlali@sii.ma>
2021-06-22 15:25:16 +0100

    Add a functional  edit and remove button | use SweetAlert

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import swal from "sweetalert";
 
 const Article = (props) => {
 	const { article } = props;
+	const [isEditing, setIsEditing] = useState(false);
+	const [editedContent, setEditedContent] = useState("");
 
 	const dateParser = (date) => {
 		let newDate = new Date(date).toLocaleDateString("fr-FR", {
@@ -14,16 +18,54 @@ const Article = (props) => {
 		return newDate;
 	};
 
+	const handleEdit = () => {
+		const data = {
+			author: article.author,
+			content: editedContent ? editedContent : article.content,
+			date: Date.now(),
+		};
+		axios.put("http://localhost:3003/articles/" + article.id, data).then();
+		setIsEditing(false);
+	};
+
+	const handleDelete = () => {
+		swal({
+			title: "Are you sure?",
+			text: "Once deleted, you will not be able to recover this Article!",
+			icon: "warning",
+			buttons: true,
+			dangerMode: true,
+		}).then((willDelete) => {
+			if (willDelete) {
+				axios.delete("http://localhost:3003/articles/" + article.id).then(window.location.reload());
+			} else {
+				swal("Your imaginary file is safe!");
+			}
+		});
+	};
+
 	return (
-		<div className="article">
+		<div className="article" style={{ background: isEditing ? "#f3feff" : "white" }}>
 			<div className="card-header">
 				<h3>{article.author}</h3>
 				<em> Posté le {dateParser(article.date)}</em>
 			</div>
-			<p>{article.content}</p>
+			{isEditing ? (
+				<textarea
+					autoFocus
+					defaultValue={editedContent ? editedContent : article.content}
+					onChange={(e) => setEditedContent(e.target.value)}
+				></textarea>
+			) : (
+				<p>{editedContent ? editedContent : article.content}</p>
+			)}
 			<div className="btn-container">
-				<button>Edit</button>
-				<button>Delete</button>
+				{isEditing ? (
+					<button onClick={handleEdit}>Save</button>
+				) : (
+					<button onClick={() => setIsEditing(true)}>Edit</button>
+				)}
+				<button onClick={handleDelete}>Delete</button>
 			</div>
 		</div>
 	);
